1e56fbc19a11b91a6da84e7381cf4302d8d645f7
naeunkim <knae11@naver.com>
2021-06-01 09:24:50 +0900

    feat: get Lines 프론트 요구사항 반영

--- src/main/java/wooteco/subway/line/dto/SimpleLineResponse.java
+++ src/main/java/wooteco/subway/line/dto/SimpleLineResponse.java
@@ -1,6 +1,7 @@
 package wooteco.subway.line.dto;
 
 import wooteco.subway.line.domain.Line;
+import wooteco.subway.station.dto.StationResponse;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,16 +11,18 @@ public class SimpleLineResponse {
     private Long id;
     private String name;
     private String color;
+    private List<StationResponse> stations;
 
-    public SimpleLineResponse(Long id, String name, String color) {
+    public SimpleLineResponse(Long id, String name, String color, List<StationResponse> stations) {
         this.id = id;
         this.name = name;
         this.color = color;
+        this.stations = stations;
     }
 
     public static List<SimpleLineResponse> listOf(List<Line> lines) {
         return lines.stream()
-                .map(line -> new SimpleLineResponse(line.getId(), line.getName(), line.getColor()))
+                .map(line -> new SimpleLineResponse(line.getId(), line.getName(), line.getColor(), StationResponse.listOf(line.getStations())))
                 .collect(Collectors.toList());
     }
 
@@ -33,5 +36,9 @@ public class SimpleLineResponse {
 
     public String getColor() {
         return color;
+    }
+
+    public List<StationResponse> getStations() {
+        return stations;
     }
 }
